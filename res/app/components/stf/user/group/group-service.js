b32b9df4cb0eb99fd3104974c6b9ed8a0ed71a7e
vdelendik <vadim.delendik@gmail.com>
2021-07-16 15:56:14 +0300

    extra loggers for invite/kick device calls

--- res/app/components/stf/user/group/group-service.js
+++ res/app/components/stf/user/group/group-service.js
@@ -9,11 +9,13 @@ module.exports = function GroupServiceFactory(
   }
 
   groupService.invite = function(device) {
+    console.log('group service invite device: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
     if (!true) {
       return Promise.reject(new Error('Device is not usable'))
     }
 
     var tx = TransactionService.create(device)
+    console.log('group service invite device2: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
     socket.emit('group.invite', device.channel, tx.channel, {
       requirements: {
         serial: {
@@ -24,20 +26,23 @@ module.exports = function GroupServiceFactory(
     })
     return tx.promise
       .then(function(result) {
+        console.log('group service invite device3: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
         return result.device
       })
       .catch(TransactionError, function() {
+        console.log('group service invite device4: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
         throw new Error('Device refused to join the group')
       })
   }
 
   groupService.kick = function(device, force) {
-    console.log('group service 1',device.usable)
+    console.log('group service kick device: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
     if (!force && !device.usable) {
       return Promise.reject(new Error('Device is not usable'))
     }
 
     var tx = TransactionService.create(device)
+    console.log('group service kick device2: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
     socket.emit('group.kick', device.channel, tx.channel, {
       requirements: {
         serial: {
@@ -48,9 +53,11 @@ module.exports = function GroupServiceFactory(
     })
     return tx.promise
       .then(function(result) {
+        console.log('group service kick device3: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
         return result.device
       })
       .catch(TransactionError, function() {
+        console.log('group service kick device4: ', device.model, '; using: ', device.using, '; usable: ', device.usable)
         throw new Error('Device refused to join the group')
       })
   }
